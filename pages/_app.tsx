d2df0bbd7935fa8ff28a97ee0fb0d02eee403aa2
Brian Prost <brian.s.prost@gmail.com>
2023-04-11 13:19:56 -0400

    new font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Hanken_Grotesk } from "@next/font/google";
+
+// fonts
+const hanken_Grotesk = Hanken_Grotesk({
+  subsets: ["latin"],  
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={hanken_Grotesk.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
