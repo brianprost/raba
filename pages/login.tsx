ff439b9e9cd0141f7ba3f76e60db2ea21454941f
Brian Prost <brian.s.prost@gmail.com>
2023-04-23 18:52:38 -0400

    idk

---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import SignInSignUp from "@/components/SignInSignUp";
+
+function Login() {
+  return (
+    <div className="flex flex-col justify-center items-center h-full">
+      <SignInSignUp />
+    </div>
+  );
+}
+
+export default Login;
