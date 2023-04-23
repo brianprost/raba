e39446e79832a59728353f4e16c46449c11a4b10
Brian Prost <brian.s.prost@gmail.com>
2023-04-23 18:47:16 -0400

    added some context

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,11 @@
-import React, { useState } from "react";
+import { useState, useContext } from "react";
 import { User } from "./types/User";
 import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { NavbarTogglesContext } from "@/pages/_app";
 
-export default function Navbar({
-  disableTitle,
-  disableAccountMenu,
-}: {
-  disableTitle?: boolean;
-  disableAccountMenu?: boolean;
-  // TODO: use these later to toggle for the login process;
-}) {
+export default function Navbar() {
+  const { showTitle, setShowTitle, showAccountMenu, setShowAccountMenu } = useContext(NavbarTogglesContext);
   const { data: session, status } = useSession();
   const [fakeUser] = useState<User>({
     id: 3,
@@ -20,51 +15,54 @@ export default function Navbar({
     organization: "Flowers",
     avatarUrl: "https://brianprost.com/img/brian-prost-scarecrow.webp",
   });
+  console.log("i'm on tha monorail")
 
   return (
     <div className="navbar h-full px-4">
       <div className="flex-1 justify-between">
-        <h1 className="text-4xl font-bold">Raba</h1>
+        <h1 className="text-4xl font-bold">{showTitle && "Raba"}</h1>
         <div className="dropdown dropdown-end">
-          {status == "authenticated" ? (
-            <div>
-              <button tabIndex={0} className="btn btn-ghost">
-                <h1 className="normal-case text-lg font-[550] pr-2">
-                  {session?.user!.email ?? "nobody"}
-                </h1>
-                <div className="avatar">
-                  <div className="w-8 rounded-full">
-                    <img
-                      src={fakeUser.avatarUrl}
-                      alt={`${fakeUser.name}'s avatar`}
-                    />
+          {showAccountMenu &&
+            (status == "authenticated" ? (
+              <div>
+                <button tabIndex={0} className="btn btn-ghost">
+                  <h1 className="normal-case text-lg font-[550] pr-2">
+                    {session?.user!.email ?? "nobody"}
+                  </h1>
+                  <div className="avatar">
+                    <div className="w-8 rounded-full">
+                      {/* eslint-disable-next-line @next/next/no-img-element */}
+                      <img
+                        src={fakeUser.avatarUrl}
+                        alt={`${fakeUser.name}'s avatar`}
+                      />
+                    </div>
                   </div>
-                </div>
-              </button>
-              <ul
-                tabIndex={0}
-                className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+                </button>
+                <ul
+                  tabIndex={0}
+                  className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    {/* TODO: link to all available links to the user */}
+                    <Link href={`/users/${session?.user!.name}`}>Profile</Link>
+                  </li>
+                  <li>
+                    <button type="button" onClick={() => signOut()}>
+                      Logout
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <button
+                type="button"
+                onClick={() => signIn("cognito")}
+                className="btn btn-ghost"
               >
-                <li>
-                  {/* TODO: link to all available links to the user */}
-                  <Link href={`/users/${session?.user!.name}`}>Profile</Link>
-                </li>
-                <li>
-                  <button type="button" onClick={() => signOut()}>
-                    Logout
-                  </button>
-                </li>
-              </ul>
-            </div>
-          ) : (
-            <button
-              type="button"
-              onClick={() => signIn("cognito")}
-              className="btn btn-ghost"
-            >
-              Login
-            </button>
-          )}
+                Login
+              </button>
+            ))}
         </div>
       </div>
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,36 @@
 import "@/styles/globals.css";
+import { useContext, createContext, useState } from "react";
 import type { AppProps } from "next/app";
 import { Hanken_Grotesk } from "@next/font/google";
 import Navbar from "@/components/Navbar";
 import { SessionProvider } from "next-auth/react";
 import CognitoProvider from "next-auth/providers/cognito";
-
+import { NavbarToggles } from "@/components/types/NavbarToggles";
 // fonts
 const hanken_Grotesk = Hanken_Grotesk({
   subsets: ["latin"],
 });
 
+export const NavbarTogglesContext = createContext({} as NavbarToggles);
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [showTitle, setShowTitle] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(true);
+  
   return (
     <SessionProvider session={pageProps.session}>
-      <main className={hanken_Grotesk.className + " bg-base-200"}>
-        <div className="h-[5vh]">
-          <Navbar />
-        </div>
-        <div className="h-[95vh]">
-          <Component {...pageProps} />
-        </div>
-      </main>
+      <NavbarTogglesContext.Provider
+        value={{showTitle, setShowTitle, showAccountMenu, setShowAccountMenu}}
+      >
+        <main className={hanken_Grotesk.className + " bg-base-200"}>
+          <div className="h-[5vh]">
+            <Navbar />
+          </div>
+          <div className="h-[95vh]">
+            <Component {...pageProps} />
+          </div>
+        </main>
+      </NavbarTogglesContext.Provider>
     </SessionProvider>
   );
 }
