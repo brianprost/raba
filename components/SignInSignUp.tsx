beaedf348dee50bdda5a1c7458509fdba8e8ff6e
Brian Prost <me@brianprost.com>
2023-07-10 14:53:56 -0400

    navbar login stuff

--- components/SignInSignUp.tsx
+++ components/SignInSignUp.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import Image from "next/image";
+import Link from "next/link";
 
 const SignInSignUp = () => {
   return (
@@ -22,6 +23,8 @@ const SignInSignUp = () => {
           className="input input-bordered w-full mb-2"
         />
         <div className="card-actions justify-end">
+        {/* <div className="card-actions justify-between items-center"> */}
+          {/* <button className="btn btn-ghost btn-xs text-xs">Forgot password</button> */}
           <button className="btn btn-primary">Sign In</button>
         </div>
         <hr className="my-8 border-gray-300" />
--- pages/account.tsx
+++ pages/account.tsx
@@ -1,10 +1,26 @@
 import SignInSignUp from '@/components/SignInSignUp'
 import React from 'react'
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function AccountPage() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
   return (
-    <div className="flex flex-col justify-center items-center h-full">
-      <SignInSignUp />
-    </div>
-  )
+    user && (
+      <div>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={user.picture!} alt={user.name!} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+
+  // return (
+  //   <div className="flex flex-col justify-center items-center h-full">
+  //     <SignInSignUp />
+  //   </div>
+  // )
 }
