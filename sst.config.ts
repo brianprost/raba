2dde476c9388c0a819d2d573553ff4fffcb27137
Brian Prost <me@brianprost.com>
2023-07-15 14:25:50 -0400

    Add environment variables for Next.js site in sst.config.ts

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,7 @@
 import { SSTConfig } from "sst";
 import { Cron, Config, Bucket, NextjsSite, Api, Table } from "sst/constructs";
+import { config } from "dotenv";
+config();
 
 export default {
   config(_input) {
@@ -39,6 +41,13 @@ export default {
       });
       const site = new NextjsSite(stack, "ui", {
         bind: [bucket, table],
+        environment: {
+          AUTH0_SECRET: process.env.AUTH0_SECRET!,
+          AUTH0_BASE_URL: app.stage == "prod" ? "https://d31ei8kk0mhc1.cloudfront.net" : process.env.AUTH0_BASE_URL!,
+          AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL!,
+          AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID!,
+          AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET!,
+        },
       });
       new Cron(stack, "cron", {
         schedule: "rate(1 day)",
