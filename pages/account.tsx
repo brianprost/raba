6e1409c25b06dd17b837e3818fc23ee148e85f02
Brian Prost <me@brianprost.com>
2023-07-16 19:07:51 -0400

    this be working but only if i hard code the email address.

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 
-export default function AccountPage() {
-  const { user, error, isLoading } = useUser();
+type AccountUploadsFromDb = {
+  uploadId: string;
+  recipientEmail: string;
+  title: string;
+  description: string;
+  chargeCode?: string;
+  fileUrl: string;
+};
 
+export default function AccountPage({
+  uploads,
+}: {
+  uploads: AccountUploadsFromDb[];
+}) {
+  const { user, error, isLoading } = useUser();
+  const [accountUploads, setAccountUploads] =
+    useState<AccountUploadsFromDb[]>();
+
+  useEffect(() => {
+    const fetchAccountUploads = async () => {
+      const res = await fetch("/api/getAccountUploads", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ senderEmail: await user?.email }),
+        // body: JSON.stringify({ senderEmail: "bprost+zepzom@humrro.org" }),
+      });
+      const uploads = await res.json();
+      return uploads;
+    };
+    fetchAccountUploads().then(setAccountUploads);
+  }, [user?.email]);
+
+  console.log("uploads: ", uploads);
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
+
   return (
     user && (
       <div className="flex h-full flex-col justify-center items-center">
@@ -19,8 +52,62 @@ export default function AccountPage() {
             <p>{user.email}</p>
           </div>
         </div>
-        <p className="text-center mt-24 text-2xl">Eventually you will be able to see all of your uploads here...</p>
+        <p className="text-center mt-24 text-2xl">
+          Eventually you will be able to see all of your uploads here...
+        </p>
+        <div className="overflow-x-auto">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Upload Id</th>
+                <th>Sender Email</th>
+                <th>Recipient Email</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Charge Code</th>
+                <th>File Url</th>
+              </tr>
+            </thead>
+            <tbody>
+              {uploads.map((upload) => (
+                <tr key={upload.uploadId}>
+                  <td>{upload.uploadId}</td>
+                  <td>{upload.recipientEmail}</td>
+                  <td>{upload.title}</td>
+                  <td>{upload.description}</td>
+                  {/* <td>{upload.chargeCode}</td> */}
+                  <td>{upload.fileUrl}</td>
+                </tr>
+              ))}
+              {/* <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr> */}
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   );
+}
+
+export async function getServerSideProps(context: any) {
+  const user = { email: "bprost+zepzom@humrro.org" };
+  const user = context.req.user;
+  const res = await fetch(`${process.env.AUTH0_BASE_URL}/api/getAccountUploads`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ senderEmail: user?.email }),
+    // body: JSON.stringify({ senderEmail: "bprost+zepzom@humrro.org" }),
+  });
+  const uploads = await res.json();
+  return {
+    props: {
+      uploads,
+    },
+  };
 }
---
+++ pages/api/getAccountUploads.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import { Table } from "sst/node/table";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const senderEmail = req.body.senderEmail;
+    console.log("senderEmail", senderEmail)
+    if (!senderEmail) {
+        return res.status(400).json({ message: "Missing email query parameter" });
+    }
+
+    const db = new DynamoDBClient({ region: "us-east-1" });
+
+    const params = {
+        TableName: Table.userUploads.tableName,
+        KeyConditionExpression: "senderEmail = :senderEmail",
+        ExpressionAttributeValues: marshall({
+            ":senderEmail": senderEmail,
+        }),
+    };
+
+    try {
+        const queryCommand = new QueryCommand(params);
+        const data = await db.send(queryCommand);
+        const items = data.Items?.map(item => unmarshall(item)) || [];
+        return res.status(200).json(items);
+    } catch (err) {
+        console.log("Error - query", err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
