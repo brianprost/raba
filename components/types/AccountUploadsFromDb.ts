4adbeb32f941cf38c469399ec3944a3ca62efeba
Brian Prost <me@brianprost.com>
2023-07-21 00:40:48 -0400

    two different types

--- components/types/AccountUploadsFromDb.ts
+++ components/types/AccountUploadsFromDb.ts
@@ -1,6 +1,17 @@
-export type AccountUploadsFromDb = {
+export type MyUpload = {
     uploadId: string;
     recipientEmail: string;
+    title: string;
+    description: string;
+    chargeCode?: string;
+    fileUrl: string;
+    createdAt: string;
+    expiresOn: string;
+};
+
+export type UploadToMe = {
+    uploadId: string;
+    senderEmail: string;
     title: string;
     description: string;
     chargeCode?: string;
