fdafa00b7d9013303e7958332436967e9059cf23
Brian Prost <me@brianprost.com>
2024-09-15 13:28:16 -0400

    init v3 sst

--- pages/api/getUploadById.ts
+++ pages/api/getUploadById.ts
@@ -1,31 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
-import { Table } from "sst/node/table";
+import { Resource } from "sst";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const uploadId = req.query.uploadId as string;
-    if (!uploadId) {
-        return res.status(400).json({ message: "Missing uploadId query parameter" });
-    }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const uploadId = req.query.uploadId as string;
+  if (!uploadId) {
+    return res
+      .status(400)
+      .json({ message: "Missing uploadId query parameter" });
+  }
 
-    const db = new DynamoDBClient({ region: "us-east-1" });
+  const db = new DynamoDBClient({ region: "us-east-1" });
 
-    const params = {
-        TableName: Table.userUploads.tableName,
-        IndexName: "uploadIdIndex",
-        KeyConditionExpression: "uploadId = :uploadId",
-        ExpressionAttributeValues: {
-            ":uploadId": { S: uploadId },
-        }
-    };
+  const params = {
+    TableName: Resource.UserUploads.name,
+    IndexName: "uploadIdIndex",
+    KeyConditionExpression: "uploadId = :uploadId",
+    ExpressionAttributeValues: {
+      ":uploadId": { S: uploadId },
+    },
+  };
 
-    try {
-        const queryCommand = new QueryCommand(params);
-        const data = await db.send(queryCommand);
-        return res.status(200).json(data.Items?.map(item => unmarshall(item))[0]);
-    } catch (err) {
-        console.log("Error - query", err);
-        return res.status(500).json({ error: err, message: "Internal server error" });
-    }
+  try {
+    const queryCommand = new QueryCommand(params);
+    const data = await db.send(queryCommand);
+    return res.status(200).json(data.Items?.map((item) => unmarshall(item))[0]);
+  } catch (err) {
+    console.log("Error - query", err);
+    return res
+      .status(500)
+      .json({ error: err, message: "Internal server error" });
+  }
 }
--- pages/api/recordToDb.ts
+++ pages/api/recordToDb.ts
@@ -1,45 +1,47 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { PutCommand } from "@aws-sdk/lib-dynamodb";
-import { Table } from "sst/node/table";
+import { Resource } from "sst";
 import type { UploadDbRecord } from "../../components/types/UploadDbRecord";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
-    try {
-        const uploadDeets = JSON.parse(req.body) as UploadDbRecord;
-        const params = {
-            TableName: Table.userUploads.tableName,
-            Key: {
-                senderEmail: uploadDeets.senderEmail,
-                uploadId: uploadDeets.uploadId,
-            },
-            Item: {
-                uploadId: uploadDeets.uploadId,
-                senderEmail: uploadDeets.senderEmail,
-                recipientEmail: uploadDeets.recipientEmail,
-                title: uploadDeets.title,
-                description: uploadDeets.description,
-                // chargeCode: uploadDeets.chargeCode,
-                fileUrl: uploadDeets.fileUrl,
-                createdAt: new Date().toISOString(),
-                expiresOn: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 7).toISOString(),
-            },
-        }
+  try {
+    const uploadDeets = JSON.parse(req.body) as UploadDbRecord;
+    const params = {
+      TableName: Resource.UserUploads.name,
+      Key: {
+        senderEmail: uploadDeets.senderEmail,
+        uploadId: uploadDeets.uploadId,
+      },
+      Item: {
+        uploadId: uploadDeets.uploadId,
+        senderEmail: uploadDeets.senderEmail,
+        recipientEmail: uploadDeets.recipientEmail,
+        title: uploadDeets.title,
+        description: uploadDeets.description,
+        // chargeCode: uploadDeets.chargeCode,
+        fileUrl: uploadDeets.fileUrl,
+        createdAt: new Date().toISOString(),
+        expiresOn: new Date(
+          new Date().getTime() + 1000 * 60 * 60 * 24 * 7,
+        ).toISOString(),
+      },
+    };
 
-        const db = new DynamoDBClient({ region: "us-east-1" });
-        try {
-            const command = new PutCommand(params);
-            const dbRes = await db.send(command);
-            console.log("Success - put", dbRes);
-            res.status(200).json({ message: "Success" })
-        } catch (err) {
-            console.log("Error - put", err);
-            res.status(500)
-        }
+    const db = new DynamoDBClient({ region: "us-east-1" });
+    try {
+      const command = new PutCommand(params);
+      const dbRes = await db.send(command);
+      console.log("Success - put", dbRes);
+      res.status(200).json({ message: "Success" });
     } catch (err) {
-        res.status(500)
+      console.log("Error - put", err);
+      res.status(500);
     }
+  } catch (err) {
+    res.status(500);
+  }
 }
